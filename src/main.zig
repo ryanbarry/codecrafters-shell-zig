fa79fac8e1b7290c118b8c602a36b445a7c3181a
Ryan Barry <ryan@nuclearice.com>
2024-11-10 19:01:02 -0800

    move type builtin to fn

--- src/main.zig
+++ src/main.zig
@@ -16,19 +16,9 @@ pub fn main() !u8 {
         const maybe_matched_cmd = std.meta.stringToEnum(Commands, first_word);
         if(maybe_matched_cmd) |matched_cmd| {
             switch (matched_cmd) {
-                .echo => try cmd_echo(split_input, stdout),
+                .echo => try cmd_echo(&split_input, stdout),
                 .exit => return std.fmt.parseInt(u8, split_input.next() orelse "0", 10),
-                .type => {
-                    const maybe_cmd_to_type = split_input.next();
-                    if (maybe_cmd_to_type) |cmd_to_type| {
-                        const maybe_ct = std.meta.stringToEnum(Commands, cmd_to_type);
-                        if (maybe_ct) |_| {
-                            try stdout.print("{s} is a shell builtin\n", .{cmd_to_type});
-                        } else {
-                            try stdout.print("{s}: not found\n", .{cmd_to_type});
-                        }
-                    }
-                }
+                .type => try cmd_type(&split_input, stdout)
             }
         } else {
             try stdout.print("{s}: command not found\n", .{user_input});
@@ -36,13 +26,21 @@ pub fn main() !u8 {
     }
 }
 
-fn cmd_echo(args: std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), stdout: std.fs.File.Writer) !void {
+fn cmd_echo(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), stdout: std.fs.File.Writer) !void {
     try stdout.print("{s}\n", .{args.rest()});
 }
 
-// fn cmd_type(args: i32, stdout: i32) void {
-
-// }
+fn cmd_type(args: *std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar), stdout: std.fs.File.Writer) !void {
+    const maybe_cmd_to_type = args.next();
+    if (maybe_cmd_to_type) |cmd_to_type| {
+        const maybe_ct = std.meta.stringToEnum(Commands, cmd_to_type);
+        if (maybe_ct) |_| {
+            try stdout.print("{s} is a shell builtin\n", .{cmd_to_type});
+        } else {
+            try stdout.print("{s}: not found\n", .{cmd_to_type});
+        }
+    }
+}
 
 fn cmdtype(cmd: Commands) CommandType {
     switch(cmd) {
